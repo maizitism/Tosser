06d327f882a7e3f5c54e8d016e8235ef13612200
Marks Maizītis <mmaizitis123@gmail.com>
2026-01-22 18:10:41 +0100

    change capitalisation of variable

--- SRC/data.hpp
+++ SRC/data.hpp
@@ -2,7 +2,7 @@
 
 struct MarkerData {
     float power = 0.0f;
-    float sweepspeed = 15.f; // px/s
+    float sweepSpeed = 15.f; // px/s
     bool charging = false;
     bool increasing = false;
 };
