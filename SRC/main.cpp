e1d3d2fff619d725ec9cca2059e3c4d78ea91f9d
Marks Maizītis <mmaizitis123@gmail.com>
2026-01-22 15:26:15 +0100

    Draw sprite and move it around

--- SRC/main.cpp
+++ SRC/main.cpp
@@ -2,9 +2,10 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode({ 800, 800 }), "SFML works!");
+    const sf::Texture texture("ASSETS/snow.png");
+    sf::Sprite sprite(texture);
+
 
     while (window.isOpen())
     {
@@ -12,10 +13,24 @@ int main()
         {
             if (event->is<sf::Event::Closed>())
                 window.close();
+
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left))
+            {
+                // left key is pressed: move our character
+                sprite.move({ -1.f, 0.f });
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right))
+            {
+                // left key is pressed: move our character
+                sprite.move({ 1.f, 0.f });
+            }
+
         }
 
         window.clear();
-        window.draw(shape);
+        window.draw(sprite);
         window.display();
+
+
     }
 }
