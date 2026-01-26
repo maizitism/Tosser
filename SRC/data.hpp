a0892a52dd950c38ad75e0140a030c7a5c39fdb2
Marks Maizītis <mmaizitis123@gmail.com>
2026-01-26 23:08:28 +0100

    change structs into namespaces

--- SRC/data.hpp
+++ SRC/data.hpp
@@ -1,21 +1,21 @@
 #pragma once
 #include "SFML/Graphics/Color.hpp"
 
-struct MarkerData {
-    float power = 0.5f; // starting value
-    float sweepSpeed = 1.5f; // power units/s
-    float throwSpeedMax = 750;
-    float throwSpeedMin = 350;
-    bool charging = false;
-    bool increasing = true;
+namespace MarkerData {
+    inline float power = 0.5f; // starting value
+    inline float sweepSpeed = 1.5f; // power units/s
+    constexpr float throwSpeedMax = 750;
+    constexpr float throwSpeedMin = 350;
+    inline bool charging = false;
+    inline bool increasing = true;
 };
 
-struct TrajectoryData {
-    float r0 = 4.0f;
-    float f = 2.0f;
+namespace TrajectoryData {
+    constexpr float r0 = 4.0f;
+    constexpr float f = 2.0f;
 };
 
-struct PowerMeterData {
-    sf::Color c_maxforce{ 216, 27, 23 }; // red color for power meter
-    sf::Color c_minforce{ 28, 124, 6 }; // green color for power meter
+namespace PowerMeterData {
+    constexpr sf::Color c_maxforce{ 216, 27, 23 }; // red color for power meter
+    constexpr sf::Color c_minforce{ 28, 124, 6 }; // green color for power meter
 };
