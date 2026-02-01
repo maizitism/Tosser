1fc754d19a67c51c76880b718f4ecdc7168efad7
Marks Maizītis <mmaizitis123@gmail.com>
2026-02-01 17:39:58 +0100

    define trajectory clamping constant

--- SRC/constants.hpp
+++ SRC/constants.hpp
@@ -6,6 +6,7 @@ namespace Const {
     constexpr float PowerMeterH = 400.f;
     constexpr float Gravity = 980.f;
     constexpr float PI = 3.1415926535f;
+    constexpr float t_clamp = 0.08f;
     constexpr float DepthFactor = 1.5f;
     constexpr float vp_x = 120.f;
     constexpr float vp_y = -80.f;
