3030376aaf647a057aadd357ed03927e2452b71e
Marks Maizītis <mmaizitis123@gmail.com>
2026-02-01 15:38:42 +0100

    add logic for ball flight

--- SRC/Ball.cpp
+++ SRC/Ball.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <algorithm>
+#include <cmath>
 #include "Ball.hpp"
 #include "constants.hpp"
 
@@ -24,6 +26,48 @@ void Ball::setPosition(sf::Vector2f pos) {
 
 sf::Vector2f Ball::getPosition() const {
     return sprite.getPosition();
+}
+
+float Ball::perspective(float time) const {
+    const float depthFactor = Const::DepthFactor;
+    const float power = 2.0f;
+
+    float k = 1.f / (1.f + std::pow(time, power) * depthFactor);
+    return std::max(k, 0.05f);
+}
+
+void Ball::throwBall(sf::Vector2f vel0, float gravity, sf::Vector2f vanishingPoint) {
+    p0 = sprite.getPosition();
+    v0 = vel0;
+    g = gravity;
+    vp = vanishingPoint;
+    
+    t = 0.f;
+    inFlight = true;
+
+    sf::Vector2f scale = sprite.getScale();
+    baseScale = { scale.x, scale.y };
+    sprite.setScale(baseScale);
+}
+
+void Ball::update(float dt) {
+    if (!inFlight) return;
+
+    t += dt;
+
+    const sf::Vector2f gvec{ 0.f, g };
+    const sf::Vector2f p = p0 + v0 * t + 0.5f * gvec * (t * t);
+
+    // Match Trajectory::rebuild EXACTLY (same k law + same clamp)
+    const float depthFactor = Const::DepthFactor;
+
+    float k = 1.f / (1.f + t * depthFactor);
+    k = std::max(k, 0.08f);
+
+    const sf::Vector2f pp = vp + (p - vp) * k;
+
+    sprite.setPosition(pp);
+    sprite.setScale({ baseScale.x * k, baseScale.y * k });
 }
 
 void Ball::draw(sf::RenderTarget& target, sf::RenderStates states) const {
--- SRC/Ball.hpp
+++ SRC/Ball.hpp
@@ -6,10 +6,24 @@ public:
     Ball();
 
     void setPosition(sf::Vector2f pos);
+    void throwBall(sf::Vector2f v0, float gravity, sf::Vector2f vanishingPoint);
+    void update(float dt);
+
     sf::Vector2f getPosition() const;
 
 private:
     sf::Texture texture;
     sf::Sprite sprite;
+
+    bool inFlight = false;
+    float t = 0.f;
+    float g = 0.f;
+    sf::Vector2f p0{};
+    sf::Vector2f v0{};
+    sf::Vector2f vp{};
+
+    sf::Vector2f baseScale{ 1.f, 1.f };
+    float perspective(float time) const;
+
     void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
 };
