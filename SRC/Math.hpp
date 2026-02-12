7c500a80050f617945d07c352203cc542d09bb02
Marks Maizītis <mmaizitis123@gmail.com>
2026-02-12 21:44:46 +0100

    remove unnecesarry helper functions

--- SRC/Math.hpp
+++ SRC/Math.hpp
@@ -4,14 +4,3 @@
 inline float lerp(float a, float b, float t) {
     return a + (b - a) * t;
 };
-
-inline float clamp01(float t) {
-    return std::clamp(t, 0.f, 1.f);
-}
-
-inline float remap(float x, float inA, float inB, float outA, float outB, bool clamp = true) {
-    float t = (x - inA) / (inB - inA);
-    if (clamp) t = clamp01(t);
-    return lerp(outA, outB, t);
-}
-
