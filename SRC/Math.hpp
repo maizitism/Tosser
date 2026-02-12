70c98a6c9a37a2c41701c78bf9dbf8ab7f727faf
Marks Maizītis <mmaizitis123@gmail.com>
2026-02-12 19:50:03 +0100

    Add additional helpers for scaling and clamping distances

--- SRC/Math.hpp
+++ SRC/Math.hpp
@@ -1,4 +1,17 @@
 #pragma once
+#include <algorithm>
 
-float lerp(float a, float b, float t);
+inline float lerp(float a, float b, float t) {
+    return a + (b - a) * t;
+};
+
+inline float clamp01(float t) {
+    return std::clamp(t, 0.f, 1.f);
+}
+
+inline float remap(float x, float inA, float inB, float outA, float outB, bool clamp = true) {
+    float t = (x - inA) / (inB - inA);
+    if (clamp) t = clamp01(t);
+    return lerp(outA, outB, t);
+}
 
