ea27aa944a67c3840db3247d2067b81eeb88db54
Marks Maizītis <mmaizitis123@gmail.com>
2026-02-18 19:57:42 +0100

    add new helper

--- SRC/Math.hpp
+++ SRC/Math.hpp
@@ -4,3 +4,8 @@
 inline float lerp(float a, float b, float t) {
     return a + (b - a) * t;
 };
+
+inline float smoothstep(float x) {
+    x = std::clamp(x, 0.f, 1.f);
+    return x * x * (3.f - 2.f * x);
+}
